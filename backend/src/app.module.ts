4ad43860e571812dcde4e58f10bfb5986cdfd104
Konstantin <k.podyganov@mail.ru>
2024-01-06 02:56:29 +0300

    feat(email): added email module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,26 @@
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { StorageModule } from './storage/storage.module';
-import { AuthModule } from './auth/auth.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { EmailModule } from './email/email.module';
+import { AuthModule } from './auth/auth.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { getMailConfig } from './common/configs/email.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.DATABASE_URL),
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMailConfig,
+    }),
     StorageModule,
     AuthModule,
     UsersModule,
+    EmailModule,
   ],
   controllers: [],
   providers: [],
--- backend/src/users/schemas/user.schema.ts
+++ backend/src/users/schemas/user.schema.ts
@@ -11,6 +11,15 @@ export class User extends Document {
   @Prop({ required: true })
   password: string;
 
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ default: false })
+  isEmailVerified: boolean;
+
+  @Prop()
+  emailVerificationToken?: string;
+
   @Prop()
   token?: string;
 }
